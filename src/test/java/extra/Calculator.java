a55522eadba90a9287dba470f69872dd0013b1aa
Ajay <ajay191987@yahoo.in>
2022-09-14 20:16:52 +0530

    Example2

--- src/test/java/extra/Calculator.java
+++ src/test/java/extra/Calculator.java
@@ -1,5 +1,14 @@
 package extra;
 
 public class Calculator {
-
+	/*
+	 * Stages in GIT
+	 * 1. Working Directory
+	 * 2. Staging Area
+	 * 3. Git Directory
+	 */
+	
+	public int addition(int a, int b) {
+		return a+b;
+	}
 }
